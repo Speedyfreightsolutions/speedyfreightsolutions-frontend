1fc340b6f83166b52cc7cf0e83ec73ce0384e044
Prince Effiong <prynzwill147@gmail.com>
2024-07-10 03:43:18 +0100

    Nav height

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,14 +4,14 @@ import MobileNav from './MobileNav';
 
 const Navbar = () => {
   return (
-    <nav className='flex-between fixed z-40 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+    <nav className='flex-between fixed h-[70px] z-40 w-full bg-dark-1 px-6 py-4 lg:px-10'>
       <Link href='/' className='flex items-center'>
         <Image
           src='/images/logo.png'
           alt='Speedy freight'
           className='max-sm:size-10'
-          width={30}
-          height={30}
+          width={80}
+          height={80}
         />
       </Link>
 
