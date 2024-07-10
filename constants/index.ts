6d30bd7a5bad0fd4dd0d44683f9c45d3d340c73d
Prince Effiong <prynzwill147@gmail.com>
2024-07-10 03:33:01 +0100

    Navbar set up

---
+++ constants/index.ts
@@ -0,0 +1,24 @@
+export const sidebarLinks = [
+      {
+        label: 'Home',
+        route: '/',
+       
+      },
+      {
+        label: 'About Us',
+        route: '/about',
+       
+      },
+    
+      {
+        label: 'Our Operations',
+        route: '/operations',
+       
+      },
+      {
+        label: 'Contact',
+        route: '/contact',
+       
+      },
+     
+    ];
